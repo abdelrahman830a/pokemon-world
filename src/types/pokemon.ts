fb8f5cc16a6c4c344204369da48899a72051a7e1
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2024-09-17 19:25:25 +0300

    Update pokemon.ts

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -35,7 +35,7 @@ export type PokemonEvolution = {
   id: number;
   name: string;
   generationId: number;
-  generation: string | any;
+  generation?: string | any;
   types: string[];
   trigger?: string;
   minLevel?: Maybe<number>;
