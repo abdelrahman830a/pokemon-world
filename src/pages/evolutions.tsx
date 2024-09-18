d597e28430d1d923a8fbf827312014053e1e4343
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2024-09-18 22:42:41 +0300

    Update evolutions.tsx

--- src/pages/evolutions.tsx
+++ src/pages/evolutions.tsx
@@ -11,6 +11,7 @@ import getQueryClient from '@/config/react-query';
 import Filter from '@/features/pokemon-evolution/components/filter';
 import PokemonEvolutionChain from '@/features/pokemon-evolution/components/pokemon-evolution-chain';
 import PokemonEvolutionChainShimmer from '@/features/pokemon-evolution/components/pokemon-evolution-chain-shimmer';
+
 import { getEvolutions, PokemonEvolutionFilter } from './api/pokemons/evolution';
 
 type Result = GetStaticPropsResult<{ dehydratedState: DehydratedState }>;
@@ -22,6 +23,7 @@ export async function getStaticProps(): Promise<Result> {
   await queryClient.fetchQuery(['pokemon-g&t'], fetchPokemonGenAndTypes);
   await queryClient.fetchQuery(['pokemon-evolution', INITIAL_FILTER, 0], () => getEvolutions({}));
 
+  // https://github.com/tannerlinsley/react-query/issues/1458
   const dehydratedState = JSON.parse(JSON.stringify(dehydrate(queryClient)));
 
   return {
@@ -36,19 +38,17 @@ export default function EvolutionsPage() {
   const [page, setPage] = useState(0);
 
   const results = useQueries({
+    // @ts-ignore
     queries: [...Array(page + 1).keys()].map((idx) => ({
-      'pokemon-evolution',
-      filter,
-      idx,
-    ] as const),
-    queryFn: fetchPokemonEvolution,
+      queryKey: ['pokemon-evolution', filter, idx],
+      queryFn: fetchPokemonEvolution,
     })),
   });
 
   const loadMoreRef = useIntersection({
     rootMargin: '560px',
-    onEnter: () => results[page]?.data?.length === 25 && setPage((prev) => prev + 1),
-    enabled: !results[page]?.isLoading,
+    onEnter: () => results[page].data!.length === 25 && setPage((prev) => prev + 1),
+    enabled: !results[page].isLoading,
   });
 
   return (
@@ -64,18 +64,16 @@ export default function EvolutionsPage() {
       <Filter filter={filter} setFilter={setFilter} />
       <hr className="-mx-6 mb-8 hidden lg:block" />
 
-      {results.map(({ data }, idx) => (
-        Array.isArray(data) && data.length > 0 ? (
-          data.map((evolution) => (
-            <PokemonEvolutionChain
-              key={evolution.map((pokemon) => pokemon.id).join('')}
-              evolution={evolution}
-            />
-          ))
-        ) : null
-      ))}
+      {results.map(({ data }) =>
+        data?.map((evolution) => (
+          <PokemonEvolutionChain
+            key={evolution.map((pokemon) => pokemon.id).join('')}
+            evolution={evolution}
+          />
+        )),
+      )}
 
-      {results[page]?.isLoading && (
+      {results[page].isLoading && (
         <>
           <PokemonEvolutionChainShimmer />
           <PokemonEvolutionChainShimmer />
