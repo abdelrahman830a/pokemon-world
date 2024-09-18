2fffffd216a2fbd1ba13f8e127e354d56b6b5d6a
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2024-09-18 22:39:39 +0300

    Update evolutions.tsx

--- src/pages/evolutions.tsx
+++ src/pages/evolutions.tsx
@@ -37,8 +37,11 @@ export default function EvolutionsPage() {
 
   const results = useQueries({
     queries: [...Array(page + 1).keys()].map((idx) => ({
-      queryKey: ['pokemon-evolution', filter, page],
-      queryFn: () => getEvolutions(filter, page),
+      'pokemon-evolution',
+      filter,
+      idx,
+    ] as const),
+    queryFn: fetchPokemonEvolution,
     })),
   });
 
