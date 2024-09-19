31acb5d80cf24c4ce9f0aa28979fc179f3f0b244
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2024-09-19 18:23:10 +0300

    Update evolution.ts

--- src/pages/api/pokemons/evolution.ts
+++ src/pages/api/pokemons/evolution.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
 import { PokemonEvolution } from '@/types/pokemon';
 import evolutions from '~/generated/pokemon-evolution.json';
 
@@ -8,7 +7,8 @@ export type PokemonEvolutionFilter = {
   type?: string;
 };
 
-const LIMIT = 128;
+const INITIAL_LIMIT = 25;
+const MAX_LIMIT = 128;
 
 export const getEvolutions = (
   { generationId, type }: PokemonEvolutionFilter,
@@ -26,16 +26,20 @@ export const getEvolutions = (
     data = data.filter((pokemons) => pokemons.some((pokemon) => pokemon.types.includes(type)));
   }
 
-  const start = LIMIT * page;
-  const end = start + LIMIT;
+  // Calculate the effective limit based on page and maximum limit
+  const limit = Math.min(INITIAL_LIMIT * (page + 1), MAX_LIMIT);
   
- // Ensure we don't exceed the length of the data
+  const start = 0; // Always start from the beginning of the data
+  const end = limit;
+  
+  // Ensure we don't exceed the length of the data
   if (start >= data.length) {
     return []; // Return an empty array if the start index is beyond the data length
   }
 
   return data.slice(start, end);
 };
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { query } = req;
