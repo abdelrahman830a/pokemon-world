a26f959705a984b4b77d15731eaf35953cbe7934
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2024-09-19 17:51:57 +0300

    Update evolution.ts

--- src/pages/api/pokemons/evolution.ts
+++ src/pages/api/pokemons/evolution.ts
@@ -8,7 +8,7 @@ export type PokemonEvolutionFilter = {
   type?: string;
 };
 
-const LIMIT = 25;
+const LIMIT = 55;
 
 export const getEvolutions = (
   { generationId, type }: PokemonEvolutionFilter,
