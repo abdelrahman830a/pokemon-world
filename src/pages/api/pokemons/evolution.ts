9f702d4b1e3888ff8b90f13505695a80191504a9
Abdelrahman Zaitoun <78018369+abdelrahman830a@users.noreply.github.com>
2024-09-19 18:36:12 +0300

    Update evolution.ts

--- src/pages/api/pokemons/evolution.ts
+++ src/pages/api/pokemons/evolution.ts
@@ -8,7 +8,7 @@ export type PokemonEvolutionFilter = {
   type?: string;
 };
 
-const LIMIT = 128;
+const LIMIT = 28;
 
 export const getEvolutions = (
   { generationId, type }: PokemonEvolutionFilter,
@@ -27,7 +27,14 @@ export const getEvolutions = (
   }
 
   const start = LIMIT * page;
-  return data.slice(start, start + LIMIT);
+  const end = Math.min(start + LIMIT, data.length);
+  
+  // Ensure we don't exceed the length of the data
+  if (start >= data.length) {
+    return []; // Return an empty array if the start index is beyond the data length
+  }
+
+  return data.slice(start, end);
 };
 
 
